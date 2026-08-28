0e6c235f5476b8b03e3eb7fb3dbdfcce84f80eeb
vishalsarkarofficial2-star <vishalsarkarofficial2@gmail.com>
2026-08-28 19:48:00 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: './',
+    base: '/AkshayB2Bsolution/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
